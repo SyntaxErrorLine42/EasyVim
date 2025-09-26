06f88fe170e7363aad20f26899ab6626af2526bf
Luka Volarevic <lvolarevic@croz.net>
2025-09-26 18:51:09 +0200

    Added luasnip as dep to cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -35,6 +35,8 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
     config = function()
       local cmp = require("cmp")
