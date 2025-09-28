b67bb1c73dd553b6d3131261e19ae4b77c4afde6
Luka <lukavolarevic11@gmail.com>
2025-09-28 12:23:01 +0200

    Added hsl colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,12 +1,13 @@
 return {
   -- Just a simple plugin for color preview when writing hex
   "catgoose/nvim-colorizer.lua",
-  event = { "BufReadPost" , "BufNewFile" },
+  event = { "BufReadPost", "BufNewFile" },
   opts = {
     user_default_options = {
       mode = "virtualtext",
       virtualtext = "󱓻", -- You need nerd fonts for this, otherwise use this: "■"
       virtualtext_inline = "before",
+      hsl_fn = true,
     },
   },
 }
