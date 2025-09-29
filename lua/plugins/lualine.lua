905e2222ac9407dc849f094e61831f04fd93a1ac
Luka <lukavolarevic11@gmail.com>
2025-09-29 13:17:49 +0200

    Added relative path to files and vim logo to mode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,40 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("lualine").setup({
-			options = {
-				-- Check https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md for all themes
-				theme = "horizon",
-                globalstatus = true, -- This makes it so that the entire nvim session uses one LuaLine
-                icons_enabled = true,
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local lualine = require("lualine")
+
+    -- Grab the default config so we don’t wipe out the other sections
+    local config = require("lualine").get_config()
+
+    config.sections.lualine_a = {
+      {
+        "mode",
+        fmt = function(str)
+          return " " .. str -- prepend Vim logo icon (nf-custom-vim in nerd fonts), so it looks cooler
+        end,
+      },
+    }
+
+    -- Override just the lualine_a section to show absolute file path
+    config.sections.lualine_c = {
+      {
+        "filename",
+        path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      },
+    }
+
+    -- Now apply the config
+    lualine.setup({
+      options = {
+        -- Check https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md for all themes
+        theme = "horizon",
+        globalstatus = true, -- This makes it so that the entire nvim session uses one LuaLine
+        icons_enabled = true,
+      },
+      sections = config.sections, -- keep defaults + our filename change
+    })
+  end,
 }
