204bdd01998c531e137c21e0f2788fdabfe424b9
Luka <lukavolarevic11@gmail.com>
2026-03-22 20:55:52 +0100

    Swapped the undotree to original vimscript plugin and neoscroll for ctrl-d and ctrl-u

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,11 +1,8 @@
 return {
 	-- I can't believe I've just started using this now
     -- Ts goated
-	"jiaoshijie/undotree",
-	opts = {
-		-- your options
-	},
-	keys = { -- load the plugin only when using it's keybinding:
-		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
-	},
+    "mbbill/undotree",
+    keys = {
+        { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle UndoTree" }
+    }
 }
