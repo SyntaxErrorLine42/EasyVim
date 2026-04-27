2ff31958900eebc953c93fc97cd811a4e0bbbdf6
Luka <lukavolarevic11@gmail.com>
2026-04-28 00:56:51 +0200

    Add images rendering for alacritty with ueberzug backend

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -3,6 +3,10 @@ return{
     build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
     ft = { "markdown" },
     opts = {
+        -- sudo apt install ueberzug
+        -- I needed this because alacritty term doesn't support images
+        -- kitty for example has this built in
+        backend = "ueberzug",
         processor = "magick_cli",
     }
 }
