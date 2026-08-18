dba0340a4a1226cc03f4f2c4a9eaed3e5ec438d1
Luka <lukavolarevic11@gmail.com>
2026-08-18 14:03:57 +0200

    fix(python): fix the bug where cache doesn't load because of the global pyenv

--- lua/plugins/python-venv.lua
+++ lua/plugins/python-venv.lua
@@ -1,6 +1,6 @@
 return {
 	-- This plugin automatically finds your python venv and applies it to the terminal and to your LSP
-    -- Call ":VenvSelect"
+	-- Call ":VenvSelect"
 	"linux-cultist/venv-selector.nvim",
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
@@ -9,10 +9,20 @@ return {
 	ft = "python",
 	opts = {
 		options = {
-            -- Disable cache. This means that every time you enter your project it will default to global python
-			enable_cached_venvs = false,
-			cached_venv_automatic_activation = false,
-			require_lsp_activation = false,
+			-- Enable cache.
+			enable_cached_venvs = true,
+			cached_venv_automatic_activation = true,
+			require_lsp_activation = true,
 		},
 	},
+    -- Fixes the bug where the global pyenv version carries into nvim session and cached env doesn't activate
+	config = function(_, opts)
+		require("venv-selector").setup(opts)
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VenvSelectActivated",
+			callback = function()
+				vim.cmd("LspRestart")
+			end,
+		})
+	end,
 }
